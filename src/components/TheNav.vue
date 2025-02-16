b174891547278fc3403a1fe99dc95819201b9f93
Nathan Albuquerque <nathan.nra@outlook.com>
2025-02-16 15:59:04 -0300

    feat: floating glass header

---
+++ src/components/TheNav.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+    <nav class="py-1.5 px-10 rounded-full space-x-10 bg-slate-200 border border-slate-300 font-montserrat font-medium tracking-wide">
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+    </nav>
+</template>
